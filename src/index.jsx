894b894527e5591f26a9ef754b616e25a8fb9895
sandeep mt <mtsandeep@gmail.com>
2017-09-08 20:39:25 +0530

    dev setup

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Home from 'containers/home';
+
+
+const renderApp = () => render(
+  <AppContainer>
+    <Home />
+  </AppContainer>,
+  document.getElementById('root'),
+);
+
+renderApp();
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept(() => renderApp());
+}
